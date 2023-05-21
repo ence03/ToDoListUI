075e682b2fe70a558d3526889523d338f1e68920
ence03 <justclarex.03@gmail.com>
2023-05-21 22:27:37 +0800

    added scheduler function

--- index.php
+++ index.php
@@ -5,11 +5,13 @@
 
     if (isset($_POST['submit'])) {
         $task = $_POST['task'];
+        $schedule = $_POST['schedule'];
         if (empty($task)) {
             $errors = "You must fill in the task";
         } else {
-            mysqli_query($db, "INSERT INTO tasks (task) VALUES ('$task')");
+            mysqli_query($db, "INSERT INTO tasks (task, schedule) VALUES ('$task', '$schedule')");
             header('location: index.php');
+            exit();
         }
     }
 
@@ -17,9 +19,10 @@
         $id = $_GET['del_task'];
         mysqli_query($db, "DELETE FROM tasks WHERE id=$id");
         header('location: index.php');
+        exit();
     }
 
-    $task = mysqli_query($db, "SELECT * FROM tasks");
+    $task = mysqli_query($db, "SELECT * FROM tasks ORDER BY id DESC");
 
 ?>
 
@@ -41,23 +44,32 @@
     <?php } ?>
 
         <input type="text" name="task" class="task-input">
+        <input type="datetime-local" name="schedule" class="task-input" id="sched" placeholder="Schedule">
         <button type="submit" class="task-btn" name="submit">Add Task</button>
     </form>
 
     <table>
         <thead>
             <tr>
-                <th>N</th>
+                <th>New</th>
                 <th>Task</th>
+                <th>Schedule</th>
                 <th>Action</th>
             </tr>
         </thead>
 
         <tbody>
         <?php $i = 1; while ($row = mysqli_fetch_array($task)) { ?>
+            <?php
+                $scheduleTime = strtotime($row['schedule']);
+                $currentTime = time();
+                $isTaskScheduled = $scheduleTime <= $currentTime;
+                $taskClass = $isTaskScheduled ? 'task scheduled' : 'task';
+            ?>
             <tr>
                 <td><?php echo $i; ?></td>
-                <td class="task"><?php echo $row['task']; ?></td>
+                <td class="task <?php echo $taskClass; ?>"><?php echo $row['task']; ?></td>
+                <td class="schedule"><?php echo $row['schedule']; ?></td>
                 <td class="delete">
                     <a href="index.php?del_task=<?php echo $row['id']; ?>">x</a>
                 </td>
@@ -67,5 +79,25 @@
         </tbody>
     </table>
     </div>
+
+    <script>
+         setTimeout(function() {
+            location.reload();
+        }, 60000);
+
+        var scheduledTasks = document.getElementsByClassName('scheduled');
+        for (var i = 0; i < scheduledTasks.length; i++) {
+            var task = scheduledTasks[i];
+            var scheduleTime = new Date(task.textContent).getTime();
+            var currentTime = new Date().getTime();
+
+            if (currentTime >= scheduleTime) {
+                var notification = document.createElement('div');
+                notification.className = 'notification';
+                notification.textContent = 'Task schedule has been triggered!';
+                task.parentNode.insertBefore(notification, task.nextSibling);
+            }
+        }   
+    </script>
 </body>
 </html>
